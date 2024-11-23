87150bd7aab37776f1983cf310477b811943db0d
Goren G <gythialy.koo+github@gmail.com>
2024-11-23 10:33:25 +0800

    fix: ensure the same alarm only send once

--- pkg/handler/info_processor.go
+++ pkg/handler/info_processor.go
@@ -223,16 +223,28 @@ func (r *InfoProcessor) Handler(i interface{}) {
 		}
 
 		// process alarms
-		var newAlarms []*model.Alarm
+		processedAlarms := make(map[string]struct{})
+		var successfulAlarms []*model.Alarm
 		for _, alarm := range pd.Alarms {
+			alarmKey := fmt.Sprintf("%d:%s", userId, alarm.CreditCode)
+
+			if _, exists := processedAlarms[alarmKey]; exists {
+				continue
+			}
+
+			alarm.UserID = userId
 			isExist, err := dal.Alarm.IsExist(userId, alarm.CreditCode)
 			if err != nil {
 				logger.Error().Err(err).Msg("failed to check alarm existence")
 				continue
 			}
+
 			if isExist {
 				continue
 			}
+
+			processedAlarms[alarmKey] = struct{}{}
+
 			if msg, err := alarm.ToMessage(); err == nil {
 				if _, msgErr := r.ctx.Bot.SendMessage(context.Background(), &bot.SendMessageParams{
 					ChatID:    userId,
@@ -240,17 +252,19 @@ func (r *InfoProcessor) Handler(i interface{}) {
 					ParseMode: models.ParseModeHTML,
 				}); msgErr != nil {
 					logger.Error().Stack().Err(msgErr).Msg("send alarm")
-				} else {
-					newAlarms = append(newAlarms, alarm)
+					continue
 				}
+
+				successfulAlarms = append(successfulAlarms, alarm)
 			} else {
 				logger.Error().Stack().Err(err).Msg("alarm to msg")
 			}
 		}
 
-		if len(newAlarms) > 0 {
-			if err := alarmDao.Insert(newAlarms); err != nil {
-				logger.Error().Stack().Err(err).Msg("")
+		// Batch insert only the alarms that were successfully sent
+		if len(successfulAlarms) > 0 {
+			if err := alarmDao.Insert(successfulAlarms); err != nil {
+				logger.Error().Stack().Err(err).Msg("batch insert alarms")
 			}
 		}
 	}
